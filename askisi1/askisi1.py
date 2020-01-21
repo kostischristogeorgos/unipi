4a753931ecc9607126b2ebf790839dfeb07375ad
kostischristogeorgos <60107236+kostischristogeorgos@users.noreply.github.com>
2020-01-21 16:03:15 +0200

    Add files via upload

--- askisi1/askisi1.py
+++ askisi1/askisi1.py
@@ -1,1 +1,18 @@
-
+with open('text.txt', 'r') as file:
+    L = file.read().split() #puts all the words from the file in the list
+L = (sorted(L, key = len, reverse = True)) #sorts the list based on the lenght of the words
+print("5 biggest words in the file")
+print("\n")
+for i in range(5):
+    print(L[i])
+print("\n")
+print("5 biggest words in the file without vowel letters")
+print("\n")
+for i in range(5):
+    L[i] = (L[i].replace('a', ''))
+    L[i] = (L[i].replace('e', ''))
+    L[i] = (L[i].replace('i', ''))
+    L[i] = (L[i].replace('o', ''))
+    L[i] = (L[i].replace('u', ''))
+    L[i] = (L[i].replace('y', ''))
+    print (L[i])
