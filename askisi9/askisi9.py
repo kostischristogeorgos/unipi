0c8cae0ee391acdfe94c11e8ad4dc5b4ccb6ae59
kostischristogeorgos <60107236+kostischristogeorgos@users.noreply.github.com>
2020-01-23 04:37:33 +0200

    Update askisi9.py

--- askisi9/askisi9.py
+++ askisi9/askisi9.py
@@ -7,17 +7,20 @@ for i in range(0, len(numlist)): #adds up all the digits in the list
     s = s + numlist[i]
 
 print("\n3*", x, "+ 1 =", number,"and the sum of its digits =",s)
-print(s, "isn't a single-digit")
-
-while(s >=10): #while the sum is larger or equal to 10, the loop continues
-    print("\n")
-    x = int(input("Give a number again"))
-    number = 3*x + 1
-    numlist = [int(x) for x in str(number)]
-    s = 0
-    for i in range(0, len(numlist)):
-        s = s + numlist[i]
-    print("\n3*", x, "+ 1 =", number,"and the sum of its digits =",s)
-    if s >= 10:
-        print("\n",s, "isn't a single-digit")
-print("\nThe program ended because the sum of the digits of",number,"is equal to",s,"which is single-digit")
+#If s < 9 then the number is a single digit
+if s < 9:
+    print(s, "is a single-digit")
+#Else it repeats the procedure
+else:
+    while(s >=10): #while the sum is larger or equal to 10, the loop continues
+        print("\n")
+        x = int(input("Give a number again"))
+        number = 3*x + 1
+        numlist = [int(x) for x in str(number)]
+        s = 0
+        for i in range(0, len(numlist)):
+            s = s + numlist[i]
+        print("\n3*", x, "+ 1 =", number,"and the sum of its digits =",s)
+        if s >= 10:
+            print("\n",s, "isn't a single-digit")
+    print("\nThe program ended because the sum of the digits of",number,"is equal to",s,"which is single-digit")
